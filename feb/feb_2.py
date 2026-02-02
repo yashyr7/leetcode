6dac3965c5e729a426ee4f84751d429880702af3
Yash Rathore <yashrath24@gmail.com>
2026-02-02 05:37:36 -0500

    feb 2

---
+++ feb/feb_2.py
@@ -0,0 +1,11 @@
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        ret = []
+
+        for n in nums:
+            ret.append(n)
+        
+        for n in nums:
+            ret.append(n)
+        
+        return ret
