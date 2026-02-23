660db12f1cc0af6ce8de1dda2c4ee9afd61d10a8
Yash Rathore <yashrath24@gmail.com>
2026-02-22 21:56:44 -0500

    fix date

--- feb/feb_21.py
+++ feb/feb_21.py
@@ -1,21 +1,16 @@
 class Solution:
-    def binaryGap(self, n: int) -> int:
+    def countPrimeSetBits(self, left: int, right: int) -> int:
         ret = 0
-        cnt = 0
-
-        while n > 0:
-            if n % 2:
+        s = {2,3,5,7,11,13,17,19}
+        for i in range(left, right + 1):
+            n = i
+            cnt = 0
+            while n > 0:
+                if n % 2:
+                    cnt += 1
                 n = n // 2
-                break
-            n = n // 2
+            
+            if cnt in s:
+                ret += 1
         
-        while n > 0:
-            cnt += 1
-            
-            if n % 2:
-                ret = max(ret, cnt)
-                cnt = 0
-            
-            n = n // 2
-
         return ret
--- feb/feb_22.py
+++ feb/feb_22.py
@@ -1,24 +1,21 @@
 class Solution:
-    def hasAllCodes(self, s: str, k: int) -> bool:
+    def binaryGap(self, n: int) -> int:
+        ret = 0
         cnt = 0
-        seen = set()
 
-        curr = ""
-        end = 0
-        while end < len(s) and end < k:
-            curr += s[end]
-            end += 1
-
-        while end < len(s):
-            if curr not in seen:
-                cnt += 1
-                seen.add(curr)
-            
-            curr = curr[1:] + s[end]
-            end += 1
+        while n > 0:
+            if n % 2:
+                n = n // 2
+                break
+            n = n // 2
         
-        if curr not in seen:
+        while n > 0:
             cnt += 1
-            seen.add(curr)
-        
-        return cnt == 2**k
+            
+            if n % 2:
+                ret = max(ret, cnt)
+                cnt = 0
+            
+            n = n // 2
+
+        return ret
