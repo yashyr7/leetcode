cc9fad7724223802793fedd6a35f949f35786669
Yash Rathore <yashrath24@gmail.com>
2026-02-27 22:44:21 -0500

    feb 28

---
+++ feb/feb_28.py
@@ -0,0 +1,7 @@
+class Solution:
+    def concatenatedBinary(self, n: int) -> int:
+        s = ""
+        for i in range(1, n + 1):
+            s = s + bin(i)[2:]
+        
+        return int(s, 2) % (10**9 + 7)
